962a96c55ccdd2d239c6bf2ce1c514af6035002b
mairura <mairurated@gmail.com>
2023-06-16 00:03:31 +0300

    register successful

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,11 @@
 import Input from "@/components/Input";
 import { useCallback, useState } from "react";
 import axios from 'axios';
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const Auth = () => {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -15,14 +18,34 @@ const Auth = () => {
     );
   }, []);
 
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
   const register = useCallback(async () => {
     try {
-        
-    } catch (error) {
-        console.log(error)
-    }
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
 
-  }, [])
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -61,7 +84,10 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700">
+            <button
+              onClick={variant == "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700"
+            >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
             <p className="text-neutral-500 mt-12">
